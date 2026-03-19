f9a2fc4b1af255ea73ed1b91f868858fe2042d27
Bryan Turns <turnsbryan@gmail.com>
2026-03-19 11:25:00 -0500

    Testing configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/barcode-db/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/barcode-db/",
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://api:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
   },
-});
+}));
